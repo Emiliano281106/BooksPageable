72307d15ab144670e328a518825964307c8b7118
albertprofe <albertprofe@gmail.com>
2024-11-04 12:00:30 +0100

    first commit

---
+++ src/main/java/io/company/library/BookRepository.java
@@ -0,0 +1,13 @@
+package io.company.library;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+//public interface BookRepository extends CrudRepository <Book, Long> {
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+
+
+    void save(Book book);
+}
+
